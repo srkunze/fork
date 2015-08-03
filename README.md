976bcc22a173c1ca9894bfbe418ac03323605cab
Sven R. Kunze <srkunze@mail.de>
2015-08-03 22:24:28 +0200

    added another 'bad'

--- README.md
+++ README.md
@@ -65,4 +65,5 @@ If necessary, decorate your functions:
 - weird calling syntax (no syntax support)
 - type(result) == BlockingFuture
 - only Python 3 for now
+- not working with lambdas
 - not thread-safe
--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     description='submitting cpu-bound tasks to processes and io-bound tasks to threads',
     author='Sven R. Kunze',
     author_email='srkunze@mail.de',
+    url='https://github.com/srkunze/fork',
     py_modules=['fork'],
 )
