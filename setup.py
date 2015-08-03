b828e1ecf02cc60c29e29fc7cee428ea3b3b2b99
Sven R. Kunze <srkunze@mail.de>
2015-08-04 00:25:59 +0200

    added classifiers

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from distutils.core import setup
 
 setup(
     name='xfork',
-    version='0.4',
+    version='0.5',
     description='submitting cpu-bound tasks to processes and io-bound tasks to threads',
     author='Sven R. Kunze',
     author_email='srkunze@mail.de',
     url='https://github.com/srkunze/fork',
     py_modules=['fork'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ],
 )
