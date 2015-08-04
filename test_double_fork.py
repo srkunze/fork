83a343a393d1695d904b3fdc6e9bad197d4ba135
Sven R. Kunze <srkunze@mail.de>
2015-08-05 00:21:27 +0200

    included @*_fork variants in test_double_fork.py

--- test_double_fork.py
+++ test_double_fork.py
@@ -16,6 +16,21 @@ def cpu_c():
     pass
 
 
+@cpu_bound_fork
+def cpu_a_fork():
+    for _ in range(30):
+        cpu_b_fork()
+
+@cpu_bound_fork
+def cpu_b_fork():
+    for _ in range(30):
+        cpu_c_fork()
+
+@cpu_bound_fork
+def cpu_c_fork():
+    pass
+
+
 @io_bound
 def io_a():
     for _ in range(30):
@@ -31,6 +46,22 @@ def io_c():
     pass
 
 
-fork(cpu_a)
-fork(io_a)
+@io_bound_fork
+def io_a_fork():
+    for _ in range(30):
+        io_b_fork()
 
+@io_bound_fork
+def io_b_fork():
+    for _ in range(30):
+        io_c_fork()
+
+@io_bound_fork
+def io_c_fork():
+    pass
+
+
+fork(cpu_a)
+cpu_a_fork()
+fork(io_a)
+io_a_fork()
