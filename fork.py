4a4121cc275dbc02716b67422419983041e6212a
Sven R. Kunze <srkunze@mail.de>
2015-08-05 14:35:57 +0200

    finished first basic contagious implementation for +

--- fork.py
+++ fork.py
@@ -6,7 +6,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 __version__ = '0.12'
 __version_info__ = (0, 12)
-__all__ = ['fork', 'cpu_bound', 'io_bound', 'cpu_bound_fork', 'io_bound_fork', 'unsafe', 'UnknownWaitingForError']
+__all__ = ['fork', 'cpu_bound', 'io_bound', 'cpu_bound_fork', 'io_bound_fork', 'contagious', 'unsafe', 'UnknownWaitingForError']
 
 
 _pools_of = threading.local()
@@ -23,10 +23,11 @@ def fork(callable_, *args, **kwargs):
     if has_side_effects:
         return callable_(*args, **kwargs)
     waiting_for = getattr(callable_, '__waiting_for__', 'cpu')
+    future_wrapper = getattr(callable_, '__future_wrapper__', FutureWrapper)
     if waiting_for == 'cpu':
-        return BlockingFuture(_pools_of.processes.submit(_safety_wrapper, callable_, *args, **kwargs))
+        return future_wrapper(_pools_of.processes.submit(_safety_wrapper, callable_, *args, **kwargs))
     elif waiting_for == 'io':
-        return BlockingFuture(_pools_of.threads.submit(_safety_wrapper, callable_, *args, **kwargs))
+        return future_wrapper(_pools_of.threads.submit(_safety_wrapper, callable_, *args, **kwargs))
     raise UnknownWaitingForError(waiting_for)
 
 
@@ -67,6 +68,8 @@ def cpu_bound_fork(callable_):
     @wraps(callable_)
     def fork_wrapper(*args, **kwargs):
         return fork(callable_, *args, **kwargs)
+    fork_wrapper.__is_fork_wrapper__ = True
+    fork_wrapper.__wrapped_callable__ = callable_
 
     import sys
     new_name = '__decorated_{name}__'.format(name=callable_.__name__)
@@ -85,7 +88,17 @@ def io_bound_fork(callable_):
     @wraps(callable_)
     def fork_wrapper(*args, **kwargs):
         return fork(callable_, *args, **kwargs)
+    fork_wrapper.__is_fork_wrapper__ = True
+    fork_wrapper.__wrapped_callable__ = callable_
     return fork_wrapper
+
+
+def contagious(callable_):
+    if getattr(callable_, '__is_fork_wrapper__', False):
+        callable_.__wrapped_callable__.__future_wrapper__ = ContagiousFutureWrapper
+    else:
+        callable_.__future_wrapper__ = ContagiousFutureWrapper
+    return callable_
 
 
 def unsafe(callable_):
@@ -101,7 +114,7 @@ def UnknownWaitingForError(Exception):
     pass
 
 
-class BlockingFuture(object):
+class FutureWrapper(object):
     
     def __init__(self, future):
         self.__future__ = future
@@ -182,7 +195,6 @@ class BlockingFuture(object):
         return item in self.__future__.result()
 
     def __add__(self, other):
-
         return self.__future__.result() + other
 
     def __sub__(self, other):
@@ -222,7 +234,7 @@ class BlockingFuture(object):
         return self.__future__.result() | other
 
     def __radd__(self, other):
-        return other + self.__future__.result()
+        return other +  self.__future__.result()
 
     def __rsub__(self, other):
         return other - self.__future__.result()
@@ -299,30 +311,245 @@ class BlockingFuture(object):
 
     def __getattribute__(self, name):
         if name == '__future__':
-            return super(BlockingFuture, self).__getattribute__('__future__')
+            return super(FutureWrapper, self).__getattribute__('__future__')
         if name == '__class__':
             return self.__future__.result().__class__
         return self.__future__.result().__getattribute__(name)
 
 
-class OpFuture(BlockingFuture):
+class ContagiousFutureWrapper(object):
+
+    def __init__(self, future):
+        self.__future__ = future
+
+    def __repr__(self):
+        return repr(self.__future__.result())
+
+    def __str__(self):
+        return str(self.__future__.result())
+
+    def __bytes__(self):
+        return bytes(self.__future__.result())
+
+    def __format__(self, format_spec):
+        return format(self.__future__.result(), format_spec)
+
+    def __lt__(self, other):
+        return self.__future__.result() < other
+
+    def __le__(self, other):
+        return self.__future__.result() <= other
+
+    def __eq__(self, other):
+        return self.__future__.result() == other
+
+    def __ne__(self, other):
+        return self.__future__.result() != other
+
+    def __gt__(self, other):
+        return self.__future__.result() > other
+
+    def __ge__(self, other):
+        return self.__future__.result() >= other
+
+    def __hash__(self):
+        return hash(self.__future__.result())
+
+    def __bool__(self):
+        return bool(self.__future__.result())
+
+    def __dir__(self):
+        return dir(self.__future__.result())
+
+    def __get__(self, instance, owner):
+        return self.__future__.result().__get__(instance, owner)
+
+    def __set__(self, instance, value):
+        return self.__future__.result().__set__(instance, value)
+
+    def __delete__(self, instance):
+        return self.__future__.result().__delete__(instance)
+
+    def __call__(self, *args, **kwargs):
+        return self.__future__.result()(*args, **kwargs)
+
+    def __len__(self):
+        return len(self.__future__.result())
+
+    def __length_hint__(self):
+        return self.__future__.result().__length_hint__()
+
+    def __getitem__(self, key):
+        return self.__future__.result()[key]
+
+    def __setitem__(self, key, value):
+        self.__future__.result()[key] = value
+
+    def __delitem__(self, key):
+        del self.__future__.result()[key]
+
+    def __iter__(self):
+        return iter(self.__future__.result())
+
+    def __reversed__(self):
+        return reversed(self.__future__.result())
+
+    def __contains__(self, item):
+        return item in self.__future__.result()
+
+    def __add__(self, other):
+        return ContagiousOperatorFutureWrapper(lambda x1, x2: x1 + x2, self.__future__, other)
+
+    def __sub__(self, other):
+        return self.__future__.result() - other
+
+    def __mul__(self, other):
+        return self.__future__.result() * other
+
+    def __truediv__(self, other):
+        return self.__future__.result() / other
+
+    def __floordiv__(self, other):
+        return self.__future__.result() // other
+
+    def __mod__(self, other):
+        return self.__future__.result() % other
+
+    def __divmod__(self, other):
+        return divmod(self.__future__.result(), other)
+
+    def __pow__(self, other, modulo=None):
+        return pow(self.__future__.result(), other, modulo)
+
+    def __lshift__(self, other):
+        return self.__future__.result() << other
+
+    def __rshift__(self, other):
+        return self.__future__.result() >> other
+
+    def __and__(self, other):
+        return self.__future__.result() & other
+
+    def __xor__(self, other):
+        return self.__future__.result() ^ other
+
+    def __or__(self, other):
+        return self.__future__.result() | other
+
+    def __radd__(self, other):
+        return ContagiousOperatorFutureWrapper(lambda x1, x2: x1 + x2, other, self.__future__)
+
+    def __rsub__(self, other):
+        return other - self.__future__.result()
+
+    def __rmul__(self, other):
+        return other * self.__future__.result()
+
+    def __rtruediv__(self, other):
+        return other / self.__future__.result()
+
+    def __rfloordiv__(self, other):
+        return other // self.__future__.result()
+
+    def __rmod__(self, other):
+        return other % self.__future__.result()
+
+    def __rdivmod__(self, other):
+        return divmod(other, self.__future__.result())
+
+    def __rpow__(self, other):
+        return pow(other, self.__future__.result())
+
+    def __rlshift__(self, other):
+        return other << self.__future__.result()
+
+    def __rrshift__(self, other):
+        return other >> self.__future__.result()
+
+    def __rand__(self, other):
+        return other & self.__future__.result()
+
+    def __rxor__(self, other):
+        return other ^ self.__future__.result()
+
+    def __ror__(self, other):
+        return other | self.__future__.result()
+
+    def __neg__(self):
+        return -self.__future__.result()
+
+    def __pos__(self):
+        return +self.__future__.result()
+
+    def __abs__(self):
+        return abs(self.__future__.result())
+
+    def __invert__(self):
+        return ~self.__future__.result()
+
+    def __complex__(self):
+        return complex(self.__future__.result())
+
+    def __int__(self):
+        return int(self.__future__.result())
+
+    def __float__(self):
+        return float(self.__future__.result())
+
+    def __round__(self, n=None):
+        return round(self.__future__.result(), n)
+
+    def __index__(self):
+        from operator import index
+        return index(self.__future__.result())
+
+    def __enter__(self):
+        return self.__future__.result().__enter__()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return self.__future__.result().__exit__(exc_type, exc_value, traceback)
+
+    def __objclass__(self):
+        return self.__future__.result().__objclass__
+
+    def __getattribute__(self, name):
+        if name == '__future__':
+            return super(ContagiousFutureWrapper, self).__getattribute__('__future__')
+        if name == '__class__':
+            return self.__future__.result().__class__
+        return self.__future__.result().__getattribute__(name)
+
+
+class ContagiousOperatorFutureWrapper(ContagiousFutureWrapper):
 
     def __init__(self, op, x1, x2):
-        self.__future__ = OpWrapper(op, x1, x2)
+        self.__future__ = OperatorFuture(op, x1, x2)
 
 
-class OpWrapper(object):
+class OperatorFuture(object):
 
     def __init__(self, op, x1, x2):
         self.op = op
         self.x1 = x1
         self.x2 = x2
+        self._cached = False
+        self._result = None
+        self._exception = None
 
     def result(self):
-        x1 = self.x1
-        if hasattr(x1, 'result'):
-            x1 = x1.result()
-        x2 = self.x2
-        if hasattr(x2, 'result'):
-            x2 = x2.result()
-        return self.op(x1, x2)
+        if not self._cached:
+            try:
+                x1 = self.x1
+                if hasattr(x1, 'result'):
+                    x1 = x1.result()
+                x2 = self.x2
+                if hasattr(x2, 'result'):
+                    x2 = x2.result()
+                self._result = self.op(x1, x2)
+            except BaseException as e:
+                self._exception = e
+            self._cached = True
+        if self._exception:
+            raise self._exception
+        else:
+            return self._result
--- test_operators.py
+++ test_operators.py
@@ -1,9 +1,16 @@
 import time
 from fork import *
 
-
-@cpu_bound
 def fib(n):
+    return 1 if n <= 1 else fib(n-1) + fib(n-2)
+
+@cpu_bound_fork
+def fib_fork(n):
+    return 1 if n <= 1 else fib(n-1) + fib(n-2)
+
+@contagious
+@cpu_bound_fork
+def fib_contagious_fork(n):
     return 1 if n <= 1 else fib(n-1) + fib(n-2)
 
 
@@ -20,7 +27,33 @@ def test_cpu_bound(n):
     start = time.time()
     par_result = 0
     for i in range(n):
-        par_result += fork(fib, i)
+        par_result += fib(i)
+    str(par_result)
+    end = time.time()
+    print('parallel:  ', end-start)
+
+    if seq_result == seq_result:
+        print('results are equal')
+    else:
+        print('results are unequal')
+        print('sequential:', seq_result)
+        print('parallel:  ', seq_result)
+
+
+def test_cpu_bound_contagious(n):
+    print('##### test_cpu_bound_contagious #####')
+    start = time.time()
+    seq_result = 0
+    for i in range(n):
+        seq_result += fib(i)
+    str(seq_result)
+    end = time.time()
+    print('sequential:', end-start)
+
+    start = time.time()
+    par_result = 0
+    for i in range(n):
+        par_result += fib_contagious_fork(i)
     str(par_result)
     end = time.time()
     print('parallel:  ', end-start)
@@ -34,3 +67,4 @@ def test_cpu_bound(n):
 
 
 test_cpu_bound(n=35)
+test_cpu_bound_contagious(n=35)
