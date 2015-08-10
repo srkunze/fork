46c9aacff34643981f86d4376abe3ccbb13d48c5
Sven R. Kunze <srkunze@tbz-pariv.de>
2015-08-10 17:00:21 +0200

    added test_exceptions

---
+++ test_exception.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import time
+from fork import *
+
+@cpu_bound_fork
+def fib_fork(n):
+    r()
+
+def r():
+    raise RuntimeError('error')
+
+
+@io_bound_fork
+def webservice_fork():
+    time.sleep(0.02)
+    raise RuntimeError('error')
+
+
+def test_cpu_bound_exception():
+    print('##### test_cpu_bound_exception #####')
+    try:
+        x = fib_fork(8)
+        print(x)
+    except Exception as exc:
+        import traceback
+        traceback.print_exc(exc)
+
+
+def test_io_bound_exception():
+    print('##### test_io_bound_exception #####')
+    try:
+        print(webservice_fork())
+    except Exception as exc:
+        import traceback
+        traceback.print_exc(exc)
+
+
+test_cpu_bound_exception()
+test_io_bound_exception()
