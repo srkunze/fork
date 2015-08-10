cf9d9825dd71342e8c42080351b01e08a0c40b28
Sven R. Kunze <srkunze@tbz-pariv.de>
2015-08-10 17:04:59 +0200

    added tests for contagious behaviour

--- test_comparison.py
+++ test_comparison.py
@@ -73,6 +73,32 @@ def test_cpu_bound_fork(n):
         print('parallel:  ', par_results)
 
 
+def test_cpu_bound_fork_contagious(n):
+    print('##### test_cpu_bound_fork_contagious #####')
+    start = time.time()
+    seq_results = 0
+    for i in range(n):
+        seq_results += fib(i)
+    str(seq_results)
+    end = time.time()
+    print('sequential:', end-start)
+
+    start = time.time()
+    par_results = 0
+    for i in range(n):
+        par_results += fib_fork(i)
+    str(par_results)
+    end = time.time()
+    print('parallel:  ', end-start)
+
+    if seq_results == par_results:
+        print('results are equal')
+    else:
+        print('results are unequal')
+        print('sequential:', seq_results)
+        print('parallel:  ', par_results)
+
+
 def test_io_bound(n):
     print('##### test_io_bound #####')
     start = time.time()
@@ -125,7 +151,35 @@ def test_io_bound_fork(n):
         print('parallel:  ', par_results)
 
 
+def test_io_bound_fork_contagious(n):
+    print('##### test_io_bound_fork_contagious #####')
+    start = time.time()
+    seq_results = ''
+    for i in range(n):
+        seq_results += webservice()
+    str(seq_results)
+    end = time.time()
+    print('sequential:', end-start)
+
+    start = time.time()
+    par_results = ''
+    for i in range(n):
+        par_results += webservice_fork()
+    str(par_results)
+    end = time.time()
+    print('parallel:  ', end-start)
+
+    if seq_results == par_results:
+        print('results are equal')
+    else:
+        print('results are unequal')
+        print('sequential:', seq_results)
+        print('parallel:  ', par_results)
+
+
 test_cpu_bound(n=30)
 test_cpu_bound_fork(n=30)
+test_cpu_bound_fork_contagious(n=30)
 test_io_bound(n=30)
 test_io_bound_fork(n=30)
+test_io_bound_fork_contagious(n=30)
