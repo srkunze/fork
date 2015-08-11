ef4c305256dfcd6aa4db7888f9b8a972c62318b1
Sven R. Kunze <srkunze@mail.de>
2015-08-11 09:51:48 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='xfork',
-    version='0.23',
+    version='0.24',
     description='submitting cpu-bound tasks to processes and io-bound tasks to threads',
     author='Sven R. Kunze',
     author_email='srkunze@mail.de',
