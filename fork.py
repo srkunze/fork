40ad5338d5d72d5a06e14f0ace1e0f3eed6d5c72
Sven R. Kunze <srkunze@tbz-pariv.de>
2015-08-28 11:52:17 +0200

    new version

--- fork.py
+++ fork.py
@@ -9,8 +9,8 @@ from concurrent.futures import Future, ProcessPoolExecutor, ThreadPoolExecutor
 
 _debug = False
 
-__version__ = '0.28'
-__version_info__ = (0, 28)
+__version__ = '0.29'
+__version_info__ = (0, 29)
 __all__ = [
     'cpu_bound', 'io_bound', 'cpu_bound_fork', 'io_bound_fork', 'unsafe',
     'fork', 'evaluate',
