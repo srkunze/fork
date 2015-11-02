7566da51e92545ca6b60d9813a1a7b0a280e4d26
Sven R. Kunze <srkunze@mail.de>
2015-11-02 23:01:37 +0100

    fixed test case

--- test_cascading_forks.py
+++ test_cascading_forks.py
@@ -21,26 +21,6 @@ def cpu_c():
     pass
 
 
-@cpu_bound_fork
-def cpu_a_fork():
-    print('cpu_a_fork')
-    for _ in range(30):
-        cpu_b_fork()
-    print('cpu_a_fork end')
-
-@cpu_bound_fork
-def cpu_b_fork():
-    print('cpu_b_fork')
-    for _ in range(30):
-        cpu_c_fork()
-    print('cpu_b_fork end')
-
-@cpu_bound_fork
-def cpu_c_fork():
-    print('cpu_c_fork')
-    pass
-
-
 @io_bound
 def io_a():
     print('io_a')
@@ -61,30 +41,9 @@ def io_c():
     pass
 
 
-@io_bound_fork
-def io_a_fork():
-    print('io_a_fork')
-    for _ in range(30):
-        io_b_fork()
-    print('io_a_fork end')
-
-@io_bound_fork
-def io_b_fork():
-    print('io_b_fork')
-    for _ in range(30):
-        io_c_fork()
-    print('io_b_fork end')
-
-@io_bound_fork
-def io_c_fork():
-    print('io_c_fork')
-    pass
-
 
 fork(cpu_a)
-cpu_a_fork()
 fork(io_a)
-io_a_fork()
 
 fork(fork, fork, fork, fork, fork, cpu_a)
 fork(fork, fork, fork, fork, fork, io_a)
