2c1625334b2bfca4f76756e4daeff33dc72a2c34
Sven R. Kunze <srkunze@mail.de>
2015-11-02 23:01:47 +0100

    fixed test case

--- test_exception.py
+++ test_exception.py
@@ -13,15 +13,6 @@ def runtime_error_cpu():
 def operator_error_cpu():
     return ''
 
-@cpu_bound_fork
-def runtime_error_cpu_fork():
-    raise RuntimeError('runtime error')
-
-@cpu_bound_fork
-def operator_error_cpu_fork():
-    return ''
-
-
 @io_bound
 def runtime_error_io():
     raise RuntimeError('runtime error')
@@ -30,17 +21,9 @@ def runtime_error_io():
 def operator_error_io():
     return ''
 
-@io_bound_fork
-def runtime_error_io_fork():
-    raise RuntimeError('runtime error')
 
-@io_bound_fork
-def operator_error_io_fork():
-    return ''
-
-
-def test_cpu_bound_runtime_error():
-    print('##### test_cpu_bound_runtime_error #####')
+def test_cpu_bound_fork_runtime_error():
+    print('##### test_cpu_bound_fork_runtime_error #####')
     try:
         x = fork(runtime_error_cpu)
         print(x)
@@ -53,24 +36,10 @@ def test_cpu_bound_runtime_error():
             print(given_traceback.strip())
 
 
-def test_cpu_bound_fork_runtime_error():
+def test_cpu_bound_process_runtime_error():
     print('##### test_cpu_bound_fork_runtime_error #####')
     try:
-        x = runtime_error_cpu_fork()
-        print(x)
-    except:
-        given_traceback = traceback.format_exc()
-        if re.match(wanted_runtime_traceback.strip(), given_traceback.strip()):
-            print('Traceback looks as desired.')
-        else:
-            print('Traceback do not look as desired:')
-            print(given_traceback.strip())
-
-
-def test_io_bound_runtime_error():
-    print('##### test_cpu_bound_runtime_error #####')
-    try:
-        x = fork(runtime_error_io)
+        x = process(runtime_error_cpu)
         print(x)
     except:
         given_traceback = traceback.format_exc()
@@ -84,7 +53,7 @@ def test_io_bound_runtime_error():
 def test_io_bound_fork_runtime_error():
     print('##### test_io_bound_fork_runtime_error #####')
     try:
-        x = runtime_error_io_fork()
+        x = fork(runtime_error_io)
         print(x)
     except:
         given_traceback = traceback.format_exc()
@@ -95,8 +64,22 @@ def test_io_bound_fork_runtime_error():
             print(given_traceback.strip())
 
 
-def test_cpu_bound_operator_error():
-    print('##### test_cpu_bound_operator_error #####')
+def test_io_bound_thread_runtime_error():
+    print('##### test_io_bound_thread_runtime_error #####')
+    try:
+        x = thread(runtime_error_io)
+        print(x)
+    except:
+        given_traceback = traceback.format_exc()
+        if re.match(wanted_runtime_traceback.strip(), given_traceback.strip()):
+            print('Traceback looks as desired.')
+        else:
+            print('Traceback do not look as desired:')
+            print(given_traceback.strip())
+
+
+def test_cpu_bound_fork_operator_error():
+    print('##### test_cpu_bound_fork_operator_error #####')
     try:
         x = 1 + fork(operator_error_cpu)
         print(x)
@@ -109,24 +92,10 @@ def test_cpu_bound_operator_error():
             print(given_traceback.strip())
 
 
-def test_cpu_bound_fork_operator_error():
-    print('##### test_cpu_bound_fork_operator_error #####')
+def test_cpu_bound_process_operator_error():
+    print('##### test_cpu_bound_process_operator_error #####')
     try:
-        x = 1 + operator_error_cpu_fork()
-        print(x)
-    except:
-        given_traceback = traceback.format_exc()
-        if re.match(wanted_operator_traceback.strip(), given_traceback.strip()):
-            print('Traceback looks as desired.')
-        else:
-            print('Traceback do not look as desired:')
-            print(given_traceback.strip())
-
-
-def test_io_bound_operator_error():
-    print('##### test_io_bound_operator_error #####')
-    try:
-        x = 1 + fork(operator_error_io)
+        x = 1 + process(operator_error_cpu)
         print(x)
     except:
         given_traceback = traceback.format_exc()
@@ -140,7 +109,21 @@ def test_io_bound_operator_error():
 def test_io_bound_fork_operator_error():
     print('##### test_io_bound_fork_operator_error #####')
     try:
-        x = 1 + operator_error_io_fork()
+        x = 1 + fork(operator_error_io)
+        print(x)
+    except:
+        given_traceback = traceback.format_exc()
+        if re.match(wanted_operator_traceback.strip(), given_traceback.strip()):
+            print('Traceback looks as desired.')
+        else:
+            print('Traceback do not look as desired:')
+            print(given_traceback.strip())
+
+
+def test_io_bound_thread_operator_error():
+    print('##### test_io_bound_thread_operator_error #####')
+    try:
+        x = 1 + thread(operator_error_io)
         print(x)
     except:
         given_traceback = traceback.format_exc()
@@ -166,7 +149,7 @@ Traceback \(most recent call last\):
       File ".*/test_exception\.py", line \d+, in <module>
         test_.*_runtime_error\(\)
       File ".*/test_exception\.py", line \d+, in test_.*_runtime_error
-        x = .*fork.*
+        x = .*runtime_error.*
       File ".*/test_exception\.py", line \d+, in runtime_error_.*
         raise RuntimeError\('runtime error'\)
     RuntimeError: runtime error
@@ -188,16 +171,16 @@ Traceback \(most recent call last\):
       File ".*/test_exception\.py", line \d+, in <module>
         test_.*_operator_error\(\)
       File ".*/test_exception\.py", line \d+, in .*_operator_error
-        x = \d+ \+ .*fork.*
+        x = \d+ \+ .*operator_error.*
     TypeError: unsupported operand type\(s\) for \+: 'int' and 'str'
 '''
 
-test_cpu_bound_runtime_error()
 test_cpu_bound_fork_runtime_error()
-test_io_bound_runtime_error()
+test_cpu_bound_process_runtime_error()
 test_io_bound_fork_runtime_error()
+test_io_bound_thread_runtime_error()
 
-test_cpu_bound_operator_error()
 test_cpu_bound_fork_operator_error()
-test_io_bound_operator_error()
+test_cpu_bound_process_operator_error()
 test_io_bound_fork_operator_error()
+test_io_bound_thread_operator_error()
